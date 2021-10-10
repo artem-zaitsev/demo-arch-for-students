01cd54fa678757c6f82e7650382c4be1abac240b
zaytsev <zaytsev@surfstudio.ru>
2021-10-10 15:11:50 +0300

    empty container

--- lib/src/di/container.dart
+++ lib/src/di/container.dart
@@ -6,15 +6,8 @@ import 'package:demo_app/src/modules/showroom/service/showroom_service.dart';
 class Di {
   static final Di instance = Di._();
 
-  late ProfileStorage profileStorage;
-  late ProfileRepository profileRepository;
-
-  late ShowroomService showroomService;
 
   Di._() {
-    profileStorage = ProfileStorage();
-    profileRepository = ProfileRepository(profileStorage);
-
-    showroomService = ShowroomService(ItemsRepository());
+    
   }
 }
